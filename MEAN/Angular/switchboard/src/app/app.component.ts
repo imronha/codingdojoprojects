4558309b464611a86f2050f978b603b5601ab9ad
Imron Hajiahmad <imronha@gmail.com>
2017-08-13 17:01:05 -0700

    finished switchboard

--- MEAN/Angular/switchboard/src/app/app.component.ts
+++ MEAN/Angular/switchboard/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  statuses = [true,true,true,true,true,true,true,true,true,true]
+  switched(i){
+      this.statuses[i] = !this.statuses[i];
+  }
 }
